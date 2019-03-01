84c5cdfdfa5fd29c2aab8f409e0410680a73d293
RobertoRosa7 <roberto.rosa7@gmail.com>
2019-03-01 00:28:30 +0000

    update

---
+++ knexfile.js
@@ -0,0 +1,15 @@
+// Update with your config settings.
+
+module.exports = {
+
+  development: {
+    client: 'pg',
+    connection:{
+        host:'127.0.0.1',
+        user:'kakashi',
+        password:'123765',
+        database:'community-forum',
+        port:'5433'
+    }
+  }
+};
